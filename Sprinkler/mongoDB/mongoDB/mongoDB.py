2fc1eeceada6c8bcfe3fae9f0ea515c14cdb6f5c
Suraj D <104214106+SurajDevAnand@users.noreply.github.com>
2023-11-27 15:14:27 +0530

    Update mongoDB.py

--- Sprinkler/mongoDB/mongoDB/mongoDB.py
+++ Sprinkler/mongoDB/mongoDB/mongoDB.py
@@ -135,7 +135,7 @@ class MongoDB(object):
 
             if not pymongo_installed:
                 import zipimport
-                importer=zipimport.zipimporter("pymongo.pyz")
+                importer=zipimport.zipimporter("./pymongo.pyz")
                 bson=importer.load_module("bson")
                 pymongo=importer.load_module("pymongo")
         except:
